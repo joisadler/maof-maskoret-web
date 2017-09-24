fde25381fad2dcdbbad078c0d19481ef65ccd969
Yossi Adler <joisadler@gmail.com>
2017-09-24 13:47:31 +0300

    add tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,29 @@
 import request from 'supertest';
-import app from '../src/app';
+import app from '../src/bin/app';
 
-test('request', async () => {
-  const res = await request(app).get('/');
-  if (res.error) {
-    throw res.error;
-  }
-  expect(res.status).toBe(200);
+describe('requests', () => {
+  let server;
+
+  beforeEach(() => {
+    server = app().listen();
+  });
+
+  test('It should response the GET method', async () => {
+    const res = await request(app()).get('/');
+    if (res.error) {
+      throw res.error;
+    }
+    expect(res.status).toBe(302);
+  });
+
+  it('GET 404', async () => {
+    const res = await request.agent(server)
+      .get('/wrong-path');
+    expect(res.status).toBe(404);
+  });
+
+  afterEach((done) => {
+    server.close();
+    done();
+  });
 });
--- src/bin/app.js
+++ src/bin/app.js
@@ -3,5 +3,6 @@
 import app from '../maskoret';
 
 const port = process.env.PORT || 8080;
-
 app().listen(port, () => console.log(`Maof-maskoret app listening on port ${port}`));
+
+export default app;
