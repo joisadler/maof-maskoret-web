adf9b62b9594e21a2a56c4cd92e9b086ea89bc4e
Yossi Adler <joisadler@gmail.com>
2017-09-25 20:24:06 +0300

    try to change port in app.js

--- src/bin/app.js
+++ src/bin/app.js
@@ -2,7 +2,7 @@
 
 import app from '../maskoret';
 
-const port = process.env.PORT || 8080;
+const port = /*process.env.PORT || */8080;
 app().listen(port, () => console.log(`Maof-maskoret app listening on port ${port}`));
 
 export default app;
